3c9f6a96df85e030ab593b603aa9b4ab381026d7
tnhiowo <jiniehuong7@gmail.com>
2025-12-02 16:39:59 +0700

    update choose role UI

--- campus-clb/src/App.js
+++ campus-clb/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Homepage from './pages/general/HomePage';
 import LoginPage from './pages/auth/LoginPage';
+import ChooseRole from './pages/general/ChooseRole';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<LoginPage  />} />
+        <Route path="/choose-role" element={<ChooseRole />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
